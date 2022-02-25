cb63c668c45c766919667d2a2475b2b68168bbfd
evanwill <evanpeterw@gmail.com>
2022-02-25 12:13:50 -0800

    messing around

--- _includes/js/cb-items.js
+++ _includes/js/cb-items.js
@@ -2,6 +2,20 @@
 var cb_items = [];
 var config_metadata = "{{ site.metadata-csv | relative_url }}";
 
+{% if page.url == '/setup/' %}
+// do the set up stuff
+
+// add listener to file selector
+var fileSelector = document.getElementById("selectFile");
+fileSelector.addEventListener('change', (event) => {
+  // give file to papa parse and init
+  var selectedFile = event.target.files[0];
+  cb_items_init(selectedFile);
+  // reload
+  location.reload();
+});
+
+{% endif %}
 // function to process items from Sheets and store
 function cb_items_init(config_metadata) {
   /* use papaparse to get metadata from configured CSV URL, then init page */
@@ -23,30 +37,8 @@ if (sessionStorage.getItem("cb_items_store")) {
 } else if (config_metadata != "") { 
   cb_items_init(config_metadata);
 } else {
-  // if no CSV is configured, ask to load local file
-  // set up select file form
-  var fileSelectCard = `<div class="w-50 mx-auto my-5">
-      <div class="card p-5" id="fileSelectCard">
-        <div class="card-body">
-          <h2>Select Metadata!</h2>
-          <p>No metadata CSV is configured in this project's "_config.yml". If you would like to test a CSV, select the file using the form below</p>
-          <div class="custom-file">
-            <input type="file" accept=".csv" class="custom-file-input" id="selectFile">
-            <label class="custom-file-label" for="selectFile">Choose metadata CSV file</label>
-          </div>
-        </div>
-      </div>
-    </div>`;
-  document.querySelector("main").innerHTML = fileSelectCard;
-  // add listener to file selector
-  var fileSelector = document.getElementById("selectFile");
-  fileSelector.addEventListener('change', (event) => {
-    // give file to papa parse and init
-    var selectedFile = event.target.files[0];
-    cb_items_init(selectedFile);
-    // reload
-    location.reload();
-  });
+  // if no CSV is configured, redirect to the set up page
+  window.location.href = "{{ '/setup/' | absolute_url }}";
 
 }
 {% if site.development-refresh == true %}
@@ -61,3 +53,9 @@ function resetStore () {
   location.reload();
 }
 {%- endif -%}
+
+
+var title = "Test Title";
+document.querySelectorAll(".site-title").forEach( e => { e.innerHTML = title; });
+var tagline = "a very good tag line";
+document.querySelectorAll(".site-tagline").forEach( e => { e.innerHTML = tagline; });
